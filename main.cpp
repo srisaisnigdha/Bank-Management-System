c1b21a97deaa0b2a2a38bf6bb7d959d69e299243
SrinidhiKavuluru <107610309+SrinidhiKavuluru@users.noreply.github.com>
2022-11-29 19:36:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "sqlite3.h"
 #ifdef _WIN32
 #include <Windows.h>
@@ -36,15 +36,15 @@ class DepositAccount
     int CIF;
     double amount;
     string type;
-    int generateAccountNumber();//RANDOM
+    int generateAccountNumber(); // RANDOM
 public:
     void createAccount();
-    void depositMoney();//.UPDATE
-    void debitMoney();//.UPDATE
+    void depositMoney(); //.UPDATE
+    void debitMoney();   //.UPDATE
     void showTransactions();
-    double getAmount(int);//.SELECT
+    double getAmount(int); //.SELECT
     void getHolderName();
-    void getAccountType();//.SELECT
+    void getAccountType(); //.SELECT
 };
 
 class LoanAccount
@@ -70,6 +70,7 @@ class LoanAccount
             return 0.097; // rete=9.7%
         }
     }
+
 public:
     void CreateNewLoan();
     void generateEMI(int principle);
@@ -79,7 +80,6 @@ public:
     void getLoanOwed();
 };
 
-
 class Bank
 {
     /*int generateCIF()
@@ -87,7 +87,7 @@ class Bank
     }*/
 
 public:
-    void newCustomer();//GENERATE CIF()
+    void newCustomer(); // GENERATE CIF()
     void searchFromCIF();
     void searchDepositAccount();
     void searchLoanAccount();
@@ -98,31 +98,32 @@ int main()
 {
     connection();
     DepositAccount a;
-    //a.createAccount();
-    //a.depositMoney();
-    //a.debitMoney();
+    // a.createAccount();
+    // a.depositMoney();
+    // a.debitMoney();
     a.getAccountType();
-    //int acc = 4000;
-    //a.getAmount(acc);
- 
+    // int acc = 4000;
+    // a.getAmount(acc);
+
     sqlite3_close(db);
     return 0;
 }
 
 void connection()
 {
-    if(sqlite3_open("bank.db", &db) == SQLITE_OK)
+    if (sqlite3_open("bank.db", &db) == SQLITE_OK)
     {
-        result = sqlite3_prepare_v2(db, "CREATE TABLE IF NOT EXISTS DEPOSITACC(ACCNO INT, TYPE TEXT, CIF INT, BALANCE REAL);",-1,&stmt,NULL);
+        result = sqlite3_prepare_v2(db, "CREATE TABLE IF NOT EXISTS DEPOSITACC(ACCNO INT, TYPE TEXT, CIF INT, BALANCE REAL);", -1, &stmt, NULL);
         sqlite3_step(stmt);
         sqlite3_finalize(stmt);
 
-        if(result != SQLITE_OK){
-            cout<<"Error: "<<sqlite3_errmsg(db)<<endl;
+        if (result != SQLITE_OK)
+        {
+            cout << "Error: " << sqlite3_errmsg(db) << endl;
         }
         else
         {
-            cout<<"Data Inserted Successfully."<<endl;
+            cout << "Data Inserted Successfully." << endl;
         }
     }
 }
@@ -130,167 +131,164 @@ void connection()
 void DepositAccount::createAccount()
 {
     char x;
-     cout << "CIF Number:";
-        cin >> CIF;
-        account_number = 13400;
-        cout << "\nSavings Account / Current Account :(S/C) ";
-        cin >> x;
-        if (x == 'S')
-        {
-            type = "SavingsAccount";
-        }
-        else
-        {
-            type = "CurrentAccount";
-        }
-        query = "INSERT INTO  DEPOSITACC(ACCNO, TYPE, CIF, BALANCE) VALUES(?,?,?,0);";
-        result=sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
-        sqlite3_bind_int(stmt, 1, account_number);
-        sqlite3_bind_text(stmt, 2, type.c_str(), type.length(), SQLITE_TRANSIENT);
-        sqlite3_bind_int(stmt,3,CIF);
+    cout << "CIF Number:";
+    cin >> CIF;
+    account_number = 13400;
+    cout << "\nSavings Account / Current Account :(S/C) ";
+    cin >> x;
+    if (x == 'S')
+    {
+        type = "SavingsAccount";
+    }
+    else
+    {
+        type = "CurrentAccount";
+    }
+    query = "INSERT INTO  DEPOSITACC(ACCNO, TYPE, CIF, BALANCE) VALUES(?,?,?,0);";
+    result = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+    sqlite3_bind_int(stmt, 1, account_number);
+    sqlite3_bind_text(stmt, 2, type.c_str(), type.length(), SQLITE_TRANSIENT);
+    sqlite3_bind_int(stmt, 3, CIF);
 
+    sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
 
-        sqlite3_step(stmt);
-        sqlite3_finalize(stmt);
-
-        if(result != SQLITE_OK){
-            cout<<"Error: "<<sqlite3_errmsg(db)<<endl;
-        }
-        else
-        {
-            cout<<"Data Inserted Successfully."<<endl;
-        }
+    if (result != SQLITE_OK)
+    {
+        cout << "Error: " << sqlite3_errmsg(db) << endl;
+    }
+    else
+    {
+        cout << "Data Inserted Successfully." << endl;
+    }
 }
 
 void DepositAccount::depositMoney()
 {
     double depo_amt;
-    cout<<"\n Type account no. to deposit money: \n";
-    cin>>account_number;
+    cout << "\n Type account no. to deposit money: \n";
+    cin >> account_number;
     getchar();
-     
-    cout<<"Enter the amount to be deposited: \n";
-    cin>>depo_amt;
-     
-     double pre_bal = getAmount(account_number);
 
-    query="UPDATE DEPOSITACC SET BALANCE = ? WHERE ACCNO = ?;";
-    result=sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
- 
-    sqlite3_bind_double(stmt,1,(depo_amt+pre_bal));
+    cout << "Enter the amount to be deposited: \n";
+    cin >> depo_amt;
+
+    double pre_bal = getAmount(account_number);
+
+    query = "UPDATE DEPOSITACC SET BALANCE = ? WHERE ACCNO = ?;";
+    result = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+
+    sqlite3_bind_double(stmt, 1, (depo_amt + pre_bal));
     sqlite3_bind_int(stmt, 2, account_number);
 
     sqlite3_step(stmt);
     sqlite3_finalize(stmt);
 
-        if(result != SQLITE_OK)
-        {
-            cout<<"FAILED: "<<sqlite3_errmsg(db)<<endl;
-        }
-        else
-        {
-            cout<<"Amount deposited Successfully."<<endl;
-            getAmount(account_number);
-        }
-
+    if (result != SQLITE_OK)
+    {
+        cout << "FAILED: " << sqlite3_errmsg(db) << endl;
+    }
+    else
+    {
+        cout << "Amount deposited Successfully." << endl;
+        getAmount(account_number);
+    }
 }
 
 void DepositAccount::debitMoney()
 {
-     double debit_amt;
-    cout<<"\n Type account no. to debit money: \n";
-    cin>>account_number;
+    double debit_amt;
+    cout << "\n Type account no. to debit money: \n";
+    cin >> account_number;
     getchar();
-     
-    cout<<"Enter the amount to be debited : \n";
-    cin>>debit_amt;
-     
-     double pre_bal = getAmount(account_number);
-     if(pre_bal < debit_amt)
-     {
-           cout<<"The balance in your account is less than the amount you need to debit !!"<<endl;
-     }
-      else
+
+    cout << "Enter the amount to be debited : \n";
+    cin >> debit_amt;
+
+    double pre_bal = getAmount(account_number);
+    if (pre_bal < debit_amt)
     {
-    query="UPDATE DEPOSITACC SET BALANCE = ? WHERE ACCNO = ?;";
-    result=sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
- 
-    sqlite3_bind_double(stmt,1,(pre_bal-debit_amt));
-    sqlite3_bind_int(stmt, 2, account_number);
+        cout << "The balance in your account is less than the amount you need to debit !!" << endl;
+    }
+    else
+    {
+        query = "UPDATE DEPOSITACC SET BALANCE = ? WHERE ACCNO = ?;";
+        result = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
 
-    sqlite3_step(stmt);
-    sqlite3_finalize(stmt);
+        sqlite3_bind_double(stmt, 1, (pre_bal - debit_amt));
+        sqlite3_bind_int(stmt, 2, account_number);
 
-        if(result != SQLITE_OK)
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+
+        if (result != SQLITE_OK)
         {
-            cout<<"FAILED: "<<sqlite3_errmsg(db)<<endl;
+            cout << "FAILED: " << sqlite3_errmsg(db) << endl;
         }
         else
         {
-            cout<<"Amount debited Successfully."<<endl;
+            cout << "Amount debited Successfully." << endl;
             getAmount(account_number);
         }
     }
 }
 
-double DepositAccount::getAmount(int account_no) //ask the account number in main before calling the get amount function.
+double DepositAccount::getAmount(int account_no) // ask the account number in main before calling the get amount function.
 {
-query="SELECT * FROM DEPOSITACC WHERE ACCNO = ?;";
+    query = "SELECT * FROM DEPOSITACC WHERE ACCNO = ?;";
 
-result=sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
-sqlite3_bind_int(stmt, 1, account_no);
+    result = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+    sqlite3_bind_int(stmt, 1, account_no);
 
-
-        if(result != SQLITE_OK)
+    if (result != SQLITE_OK)
+    {
+        cout << "ERROR: " << sqlite3_errmsg(db) << endl;
+    }
+    else
+    {
+        while (sqlite3_step(stmt) == SQLITE_ROW)
         {
-            cout<<"ERROR: "<<sqlite3_errmsg(db)<<endl;
-        }
-        else
-        {
-              while(sqlite3_step(stmt)==SQLITE_ROW)
-            {
-                          double r = sqlite3_column_double(stmt,3);
-            cout<<"The balance in your account is: "<<r<<endl;
+            double r = sqlite3_column_double(stmt, 3);
+            cout << "The balance in your account is: " << r << endl;
             return r;
-            }
-              cout<<"The account with given account number does not exist "<<endl;          
         }
-        return 0;
+        cout << "The account with given account number does not exist " << endl;
+    }
+    return 0;
 }
 void DepositAccount::getAccountType()
 {
-      cout<<"Enter the account number to get the given account type : ";
-      cin>>account_number;
-      getchar();
-     
-      query="SELECT * FROM DEPOSITACC WHERE ACCNO = ?;";
+    cout << "Enter the account number to get the given account type : ";
+    cin >> account_number;
+    getchar();
 
-result=sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
-sqlite3_bind_int(stmt, 1, account_number);
+    query = "SELECT * FROM DEPOSITACC WHERE ACCNO = ?;";
 
+    result = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+    sqlite3_bind_int(stmt, 1, account_number);
 
-        if(result != SQLITE_OK)
-        {
-            cout<<"ERROR: "<<sqlite3_errmsg(db)<<endl;
-        }
-        else
-        {
-              while(sqlite3_step(stmt)==SQLITE_ROW)
-            {
-            cout<<"your account type is: "<<sqlite3_column_text(stmt,1)<<endl;
-            return;
-            }
-             cout<<"The account with given account number does not exist "<<endl;          
-        }
-}
-    void LoanAccount::generateEMI(int principle)
+    if (result != SQLITE_OK)
     {
-        double emi_amt, i;
-        i = getInterestRate();
-        i = i / (12 * 100);   // one month interest
-        period = period * 12; // one month period
-        emi_amt = (principle * i * pow(1 + i, period)) / (pow(1 + i, period) - 1);
+        cout << "ERROR: " << sqlite3_errmsg(db) << endl;
     }
+    else
+    {
+        while (sqlite3_step(stmt) == SQLITE_ROW)
+        {
+            cout << "your account type is: " << sqlite3_column_text(stmt, 1) << endl;
+            return;
+        }
+        cout << "The account with given account number does not exist " << endl;
+    }
+}
+void LoanAccount::generateEMI(int principle)
+{
+    double emi_amt, i;
+    i = getInterestRate();
+    i = i / (12 * 100);   // one month interest
+    period = period * 12; // one month period
+    emi_amt = (principle * i * pow(1 + i, period)) / (pow(1 + i, period) - 1);
+}
 void LoanAccount::CreateNewLoan()
 {
     char x;
@@ -308,26 +306,25 @@ void LoanAccount::CreateNewLoan()
         type = "PropertyLoan";
     }
     query = "INSERT INTO  LOANACC(ACCNO, TYPE, CIF,DATEISSUED,PERIOD,DUEDATE, TRANSACTIONS) VALUES(?,?,?,?);";
-    result=sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+    result = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
     sqlite3_bind_int(stmt, 1, account_number);
     sqlite3_bind_text(stmt, 2, type.c_str(), type.length(), SQLITE_TRANSIENT);
-    sqlite3_bind_int(stmt,3,CIF);
-    sqlite3_bind_int(stmt,4,d);
+    sqlite3_bind_int(stmt, 3, CIF);
+    sqlite3_bind_int(stmt, 4, d);
     sqlite3_bind_text(stmt, 5, type.c_str(), type.length(), SQLITE_TRANSIENT);
-
 
     sqlite3_step(stmt);
     sqlite3_finalize(stmt);
 
-    if(result != SQLITE_OK){
-        cout<<"Error: "<<sqlite3_errmsg(db)<<endl;
+    if (result != SQLITE_OK)
+    {
+        cout << "Error: " << sqlite3_errmsg(db) << endl;
     }
     else
     {
-        cout<<"Data Inserted Successfully."<<endl;
+        cout << "Data Inserted Successfully." << endl;
     }
 }
 void Bank::newCustomer()
 {
-   
 }
